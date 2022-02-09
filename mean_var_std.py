acd2980c9592347dda35ab3c149fcc55f5cd3130
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-02-09 12:22:33 -0500

    Mean fuction added

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,21 @@ import numpy as np
 
 def calculate(list):
 
+##  Reshaping the list to a matrix
+  my_array = np.array(list)
+  my_matrix = my_array.reshape(3,3)
+
+  
+  print("AQUI")
+  print(mean(my_matrix))
 
 
+  # return calculations
 
-    return calculations
+def mean(matrix):
+
+  axis1 = matrix.mean(axis = 0)
+  axis2 = matrix.mean(axis = 1)
+  flattened = (axis1.mean() + axis2.mean()) / 2
+
+  return [list(axis1), list(axis2), flattened]
